158011320bb5408e849fbe04a3eea5b2669e993b
k3huang <49205402+k3huang@users.noreply.github.com>
2021-02-04 21:45:40 -0800

    Update run.py

--- run.py
+++ run.py
@@ -9,9 +9,10 @@ warnings.filterwarnings('ignore')
 
 
 sys.path.insert(0, 'src/data')
-from Loading_Data import load_data
-sys.path.insert(0, 'src/analysis')
-import analysis 
+from Loading_Data import * 
+sys.path.insert(0, 'src/eda')
+from feature_selection import * 
+
 
 
 def main(targets):
@@ -26,11 +27,20 @@ def main(targets):
         with open('config/data-params.json') as fh:
             data_cfg = json.load(fh)
         # make the data target
-        battery5 = load_data()
-        display(battery5)
-#         return battery_information
-    
-    if 'analysis' in targets:
+        device_use = load_device()
+        battery_event = load_battery_event()
+        battery_info = load_battery_info()
+        process = load_process()
+        cpu = load_cpu()
+        
+        display(device_use)
+        display(battery_event)
+        display(battery_info)
+        display(process)
+        display(cpu)
+
+        
+    if 'eda' in targets:
         ## Data Preprocessing Part
         
         try:
@@ -45,7 +55,7 @@ def main(targets):
         
         return ridge_score, logistic_score 
     
-    if 'test' in targets:
+    if 'model' in targets:
         with open('config/data-params.json') as fh:
             data_cfg = json.load(fh)
         # make the data target
